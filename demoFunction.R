090a19bb63f2da4ae36e41b7cc845159ede3e6c0
Meghan O'Neill <meghan.r.oneill@gmail.com>
2014-11-05 13:07:23 -0800

    Remove NSE, Template formatting
    Change subset statement to [] operator to avoid NSE problems in function
    Use 6 spaces instead of tab.

--- demoFunction.R
+++ demoFunction.R
@@ -1,7 +1,7 @@
 # HEADER-----------------------------------------------------------------------
 #
-# Demo Function 
-#
+# Function Name: doPlotSVC 
+# Project: 123299000 700 Technical Development
 # Author: Meghan O'Neill
 #
 # Purpose: Piece of code to use in test-repo to test out GitHub and bare
@@ -16,27 +16,29 @@
 # FUNCTION----------------------------------------------------------------------
 
 doPlotSVC <- function(svc_name) {
-  # Just get the records for this service
-  temp_df <- subset(sched_df, Service == svc_name)
-  
-  # Create the plot object
-  ggobj <- ggplot(
-    temp_df, 
-    aes(x = ScheduledDaysInAdvance)
-  ) + 
-  geom_histogram(aes(y = ..density..), binwidth = 4)
-  
-  # Add some titles and axis labels
-  ggobj <- ggobj + 
-    ggtitle(svc_name) + 
-    xlab("Scheduled Days in Advance") + 
-    ylab("Frequency")
-  
-  # Need to use print() to actually produce the plot
-  print(ggobj)
-  
-  # Save to PDF
-  ggsave(sprintf("plot/%s.pdf", svc_name))
-  
-  return (ggobj)
+      # Just get the records for this service
+      temp_df <- sched_df[sched_df$Service == svc_name, ]
+      
+      # Create the plot object
+      ggobj <- ggplot(
+            temp_df, 
+            aes(x = ScheduledDaysInAdvance)
+      ) + 
+            geom_histogram(aes(y = ..density..), binwidth = 4)
+      
+      # Add some titles and axis labels
+      ggobj <- ggobj + 
+            ggtitle(svc_name) + 
+            xlab("Scheduled Days in Advance") + 
+            ylab("Frequency")
+      
+      # Print() to actually produce the plot
+      print(ggobj)
+      
+      # Save to PDF
+      ggsave(sprintf("plot/%s.pdf", svc_name))
+      
+      return (ggobj)
 }
+
+# END---------------------------------------------------------------------------
--- script_demo_script.R
+++ script_demo_script.R
@@ -1,33 +1,32 @@
-# Header -----------------------------------------------------------------------
+# HEADER -----------------------------------------------------------------------
 #
-# Demo Script 
-#
+# Project: 123299000 700 Technical Development
 # Author: Meghan O'Neill
 #
-# Purpose: Script that uses demoFunction in test-repo to test out GitHub
-# and bare repos on the network
+# Purpose: This demo script creates ggplots from the SchedDaysAdv.csv file
+# and saves them in a list and as pdfs using the function doPlotSVC.
 #
-# Demo script from; 
+# Original code from; 
 # http://hselab.org/content/create-sequence-plots-r-and-ggplot2-and-save-pdfs
 #
-# Setup ------------------------------------------------------------------------
+# SETUP ------------------------------------------------------------------------
 
-# get library
+# Libraries
 require(ggplot2)
 
-# get data
+# Data
 sched_df <- read.csv("SchedDaysAdv.csv")
 
 # remove '/' from Service in data file (causes errors)
 levels(sched_df$Service)[levels(sched_df$Service) == "Urology/GU Surgery"] <- 
-  "Urology GU Surgery"
+      "Urology GU Surgery"
 
-# source function
+# Source function(s)
 source("demoFunction.R")
 
-# Script -----------------------------------------------------------------------
+# SCRIPT -----------------------------------------------------------------------
 
-# get plots, printed as pdfs and saved in list
+# get plots, one for each Service type, printed as pdfs and saved in list
 svc <- lapply(unique(sched_df$Service), doPlotSVC)
 
 # END --------------------------------------------------------------------------
